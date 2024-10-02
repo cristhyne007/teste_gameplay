de00b2913e937c8aa811163673ef397ad23f3790
cristhyne007 <160241007+cristhyne007@users.noreply.github.com>
2024-10-02 18:03:10 -0300

    Update App.tsx

--- game.rle/App.tsx
+++ game.rle/App.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, 
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'end',
     backgroundColor: 'black',
   },
   box1: {
